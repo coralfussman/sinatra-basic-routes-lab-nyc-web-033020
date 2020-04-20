71b770d2e1f34a908f230dd4a47d80723cc92cad
coralfussman <coralfussman@gmail.com>
2020-04-20 10:48:51 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        @@status = "200"
+        @@status +  "My name is Coral"
+    end
+
+    get '/hometown' do
+        @@status + "My hometown is TLV"
+    end
+
+        get '/favorite-song' do
+            @@status + "My favorite song is Cheek to Cheek"
+        end
+
+       
+
+
 end
